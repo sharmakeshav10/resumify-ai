0700e262afdd02f65dbd283ee4e36be23f94c91a
Keshav Sharma <sharmakeshav0708@gmail.com>
2024-12-30 01:57:49 +0530

    implemented ai for summary generation

--- src/components/resume/form/SummaryForm.jsx
+++ src/components/resume/form/SummaryForm.jsx
@@ -6,14 +6,33 @@ import { Loader, Sparkles } from "lucide-react";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast, useToast } from "@/hooks/use-toast";
+import { chatSession } from "@/service/AiService";
+
+const prompt =
+  "Job Title: {jobTitle} , Depends on job title give me list of  summery for 3 experience level, Mid Level and Freasher level in 3 -4 lines in array format, With summery and experience_level Field in JSON Format";
 
 const SummaryForm = ({ enabledNext }) => {
   const { resumeInfo, updateResume } = useResume();
   const [isLoading, setIsLoading] = useState(false);
   const [summary, setSummary] = useState("");
+  const [aiSummary, setAiSummary] = useState();
   const { resumeId } = useParams();
 
   const { toast } = useToast();
+
+  const aiGeneratedSummary = async () => {
+    setIsLoading(true);
+    try {
+      const PROMPT = prompt.replace("jobTitle", resumeInfo?.jobTitle);
+      const result = await chatSession.sendMessage(PROMPT);
+      console.log("AI Response Text: ", result.response.text());
+      setAiSummary(JSON.parse(result.response.text()));
+      setIsLoading(false);
+    } catch (e) {
+      console.log("AI couldnt generate summary due to: ", e);
+      setIsLoading(false);
+    }
+  };
 
   const handleSummaryChange = (e) => {
     console.log(e.target.value);
@@ -56,40 +75,63 @@ const SummaryForm = ({ enabledNext }) => {
   };
 
   return (
-    <div className="border shadow-lg rounded-lg p-6 max-w-4xl mx-auto">
-      <h2 className="font-bold text-2xl ">About Me</h2>
-      <p className="font-semibold text-lg  mt-1">Tell Us About Yourself</p>
+    <>
+      <div className="border shadow-lg rounded-lg p-6 max-w-4xl mx-auto">
+        <h2 className="font-bold text-2xl ">About Me</h2>
+        <p className="font-semibold text-lg  mt-1">Tell Us About Yourself</p>
 
-      <form onSubmit={handleSaveSummary} className="mt-6">
-        <div className="flex justify-between items-end">
-          <p>Add Summary</p>
-          <Button
-            variant="outline"
-            type="button"
-            className="border-teal-600 text-teal-600 hover:bg-teal-600 hover:text-white rounded"
-          >
-            <Sparkles />
-            Summarize with AI
-          </Button>
-        </div>
-        <Textarea
-          onChange={handleSummaryChange}
-          required
-          name="summary"
-          className="p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-teal-600 mt-5"
-        />
+        <form onSubmit={handleSaveSummary} className="mt-6">
+          <div className="flex justify-between items-end">
+            <p>Add Summary</p>
+            <Button
+              variant="outline"
+              type="button"
+              onClick={aiGeneratedSummary}
+              className="border-teal-600 text-teal-600 hover:bg-teal-600 hover:text-white rounded"
+            >
+              <Sparkles />
+              Summarize with AI
+            </Button>
+          </div>
+          <Textarea
+            onChange={handleSummaryChange}
+            required
+            name="summary"
+            value={summary}
+            defaultValue={summary ? summary : resumeInfo?.summary}
+            className="p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-teal-600 mt-5"
+          />
 
-        <div className="flex justify-end mt-6">
-          <Button
-            disabled={isLoading}
-            type="submit"
-            className="bg-teal-600 text-white hover:bg-teal-700 focus:ring-4 focus:ring-teal-300"
-          >
-            {isLoading ? <Loader /> : "Save"}
-          </Button>
+          <div className="flex justify-end mt-6">
+            <Button
+              disabled={isLoading}
+              type="submit"
+              className="bg-teal-600 text-white hover:bg-teal-700 focus:ring-4 focus:ring-teal-300"
+            >
+              {isLoading ? <Loader /> : "Save"}
+            </Button>
+          </div>
+        </form>
+      </div>
+
+      {aiSummary && (
+        <div className="my-5">
+          <h2 className="font-bold text-lg">Suggestions</h2>
+          {aiSummary?.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => setSummary(item?.summary)}
+              className="p-5 shadow-lg my-4 rounded-lg cursor-pointer"
+            >
+              <h2 className="font-bold my-1 text-primary">
+                Level: {item?.experience_level}
+              </h2>
+              <p>{item?.summary}</p>
+            </div>
+          ))}
         </div>
-      </form>
-    </div>
+      )}
+    </>
   );
 };
 
